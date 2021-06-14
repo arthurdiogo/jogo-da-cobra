5a262213b2a11b4105189321bbf86f025cd74525
dioguincrvg <arthurdiogo1995@gmail.com>
2021-06-14 11:55:19 -0300

    definindo reações e impedindo que a cobra saia da tela

--- script.js
+++ script.js
@@ -20,7 +20,22 @@ function criarCobra(){
     }
 }
 
-function iniciarJogo(){
+document.addEventListener('keydown', update); //reconhece eventos do teclado
+
+function update (event){
+    if(event.keyCode == 37 && direcao != "direita") (direcao = "esquerda");
+    if(event.keyCode == 38 && direcao != "baixo") (direcao = "cima");
+    if(event.keyCode == 39 && direcao != "esquerda") (direcao = "direita");
+    if(event.keyCode == 40 && direcao != "cima") (direcao = "baixo");
+
+}
+
+function iniciarJogo(){ //para cobra voltar para a tela quando chegar no final
+    if(cobra[0].x > 16 * box && direcao == "direita") cobra[0].x = 0;
+    if(cobra[0].x < 0 && direcao == "esquerda") cobra[0].x = 16 * box;
+    if(cobra[0].y > 16 * box && direcao == "baixo") cobra[0].y = 0;
+    if(cobra[0].y < 0 && direcao == "cima") cobra[0].y = 16 * box;
+
     criarBG();
     criarCobra();
 
